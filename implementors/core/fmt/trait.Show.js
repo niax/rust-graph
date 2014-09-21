dfa5f793dfafb43f6c18a19cf44af714e747db9f
Nicholas Hollett <niax@niax.co.uk>
2014-09-21 20:08:33 +0100

    Initial doc import.

---
+++ implementors/core/fmt/trait.Show.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['graph'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a, T: <a class='trait' href='http://doc.rust-lang.org/master/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a>, V: <a class='trait' href='http://doc.rust-lang.org/master/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a>&gt; <a class='trait' href='http://doc.rust-lang.org/master/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='graph/struct.Graph.html' title='graph::Graph'>Graph</a>&lt;'a, T, V&gt;","<a class='stability Unmarked' title='No stability level'></a>impl&lt;V: <a class='trait' href='http://doc.rust-lang.org/master/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a>&gt; <a class='trait' href='http://doc.rust-lang.org/master/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='graph/struct.Edge.html' title='graph::Edge'>Edge</a>&lt;V&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
